282418bab988ee1fd7fb0c13d62fd443b782b003
Raphael Jay Bernardo <73016403+hackwebdev@users.noreply.github.com>
2022-08-15 19:22:18 +0800

    add functionality

---
+++ README.md
@@ -0,0 +1,20 @@
+- react icons
+- local data
+
+```
+const reviews = [
+    {},{},{}
+]
+
+export default reviews
+```
+
+access the array
+destructure array
+`const { name, job, image, text } = people[index]`
+
+Carousel
+
+- previous button
+- next button
+- random button
--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
-import Review from './Review'
+import Review from './components/Review'
 import './App.css'
 
 const App = () => {
-  return <h2>reviews project setup</h2>
+  return (
+    <main>
+      <section className='container'>
+        <div className='title'>
+          <h2>Our Reviews</h2>
+          <div className='underline'></div>
+        </div>
+        <Review />
+      </section>
+    </main>
+  )
 }
 
 export default App
